fb37cd68dd89eced4f12ff3ccb5935265bfc028d
Websi <web.sidehustle2025@gmail.com>
2025-08-07 20:37:00 +0530

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,39 @@
-import React from 'react'
-import {Button} from "@/components/ui/button";
 import CompanionCard from "@/components/CompanionCard";
 import CompanionsList from "@/components/CompanionsList";
 import CTA from "@/components/CTA";
 import {recentSessions} from "@/constants";
+import {getAllCompanions, getRecentSessions} from "@/lib/actions/companion.actions";
+import {getSubjectColor} from "@/lib/utils";
 
-const Page = () => {
+const Page = async () => {
+    const companions = await getAllCompanions({ limit: 3 });
+    const recentSessionsCompanions = await getRecentSessions(10);
 
     return (
-      <main>
-          <h1 className="text-2xl ">Popular Companions</h1>
+        <main>
+            <h1>Popular Companions</h1>
 
+            <section className="home-section">
+                {companions.map((companion) => (
+                    <CompanionCard
+                        key={companion.id}
+                        {...companion}
+                        color={getSubjectColor(companion.subject)}
+                    />
+                ))}
 
-          <section className="home-section">
-              <CompanionCard
-                  id={'123'}
-                  name={'Neura the Brainy Expert'}
-                  topic={'Neural Network of The Brain'}
-                  subject={'science'}
-                  duration={45}
-                  color={"#ffda6e"}
-                  bookmarked={false} />
+            </section>
 
-              <CompanionCard
-                  id={'456'}
-                  name={'Countsy the Number Wizard'}
-                  topic={' Derivatives & Integrals'}
-                  subject={'Mathematics'}
-                  duration={30}
-                  color={"#e5d0ff"}
-                  bookmarked={false} />
-
-              <CompanionCard
-                  id={'789'}
-                  name={'Verba the Vocabulary Builder'}
-                  topic={'English Literature'}
-                  subject={'Language'}
-                  duration={30}
-                  color={"#BDE755"}
-                  bookmarked={false} />
-          </section>
-          <section className='home-section'>
-              <CompanionsList title={'Recently Completed Sessions'}
-              companions={recentSessions}
-              classNames="w-2/3 max-lg:w-full"
-              />
-              <CTA/>
-          </section>
-
-      </main>
-  )
+            <section className="home-section">
+                <CompanionsList
+                    title="Recently completed sessions"
+                    companions={recentSessionsCompanions}
+                    classNames="w-2/3 max-lg:w-full"
+                />
+                <CTA />
+            </section>
+        </main>
+    )
 }
 
 export default Page
--- components/CompanionCard.tsx
+++ components/CompanionCard.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import {addBookmark, removeBookmark} from "@/lib/actions/companion.actions";
 
 interface CompanionCardProps {
     id: string;
@@ -25,27 +24,12 @@ const CompanionCard = ({
                            bookmarked,
                        }: CompanionCardProps) => {
     const pathname = usePathname();
-    const handleBookmark = async () => {
-        if (bookmarked) {
-            await removeBookmark(id, pathname);
-        } else {
-            await addBookmark(id, pathname);
-        }
-    };
+
     return (
         <article className="companion-card" style={{ backgroundColor: color }}>
             <div className="flex justify-between items-center">
                 <div className="subject-badge">{subject}</div>
-                <button className="companion-bookmark" onClick={handleBookmark}>
-                    <Image
-                        src={
-                            bookmarked ? "/icons/bookmark-filled.svg" : "/icons/bookmark.svg"
-                        }
-                        alt="bookmark"
-                        width={12.5}
-                        height={15}
-                    />
-                </button>
+
             </div>
 
             <h2 className="text-2xl font-bold">{name}</h2>
